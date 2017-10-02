95713cfb6f045e68676631917414e75b768e2486
Amanda George <github email address>
2017-10-02 12:33:01 -0500

    create fetch for posts

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch'
 //select subreddit to display or refresh subreddit
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 
@@ -35,5 +36,15 @@ export function receivePosts(subreddit, json) {
     type: RECEIVE_POSTS,
     subreddit,
     posts: json.data.children.map(child =>child.data), receivedAt: Date.now()
+  }
+}
+
+export function fetchPosts(subreddit) {
+  return function (dispatch){
+    dispatch(requestPosts(subreddit))
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+    .then(resp => resp.json(),
+      error => console.log('ERROR', error))
+    .then(json => dispatch(receivePosts(subreddit, json)))
   }
 }
