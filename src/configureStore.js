1085127f4312f8075ea22d69542dcba032572f88
Amanda George <github email address>
2017-10-02 13:52:14 -0500

    remove create store to own file

---
+++ src/configureStore.js
@@ -0,0 +1,14 @@
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+
+
+export default function configureStore(preloadedState) {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(
+      thunkMiddleware
+    )
+  }
+)
