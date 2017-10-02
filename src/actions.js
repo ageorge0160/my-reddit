b6a8f45952a287fc860631f137b0a7f68fef2c73
Amanda George <github email address>
2017-10-02 10:22:22 -0500

    action for selecting subreddit

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+//select subreddit to display or refresh subreddit
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+
+export function selectSubreddit(subreddit) {
+  return {
+    type: SELECT_SUBREDDIT,
+    subreddit
+  }
+}
