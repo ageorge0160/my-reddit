e844e893b474d798d8e8f41e9c50d2a7cfe4c916
Amanda George <github email address>
2017-10-02 15:25:54 -0500

    create subfilter

---
+++ src/components/SubFilter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+export default class SubFilter extends Component {
+  render() {
+    const {value, onChange, options} = this.props
+    return(
+      <div>
+        <h1>{value}</h1>
+          <select onChange={event => onChange(event.target.value)} value={value}>
+            {options.map(option => (
+              <option value={option} key={option}>
+                {option}
+              </option>
+          ))}
+        </select>
+      </div>
+    )
+  }
+}
+
+SubFilter.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}
