d5002368308571b18827fd60408bf4febdf8ddce
Amanda George <github email address>
2017-10-02 10:25:39 -0500

    invalidate subreddit function

--- src/actions.js
+++ src/actions.js
@@ -7,3 +7,12 @@ export function selectSubreddit(subreddit) {
     subreddit
   }
 }
+
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
+
+export function invalidateSubreddit(subreddit) {
+  return {
+    type: INVALIDATE_SUBREDDIT,
+    subreddit
+  }
+};
