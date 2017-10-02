93df82ce0ed4edcfcc8a9ed123dddbb2430e2291
Amanda George <github email address>
2017-10-02 12:35:25 -0500

    add imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import {selectSubreddit, fetchPosts } from './actions'
+import rootReducer from './reducers'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
