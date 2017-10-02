70a51dc849e83a0ee88705b713994de7e5495098
Amanda George <github email address>
2017-10-02 14:06:56 -0500

    add proptype requirement

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Posts extends Component {
   render() {
     return (
       <ul>
-        list of posts
+        {this.props.posts.map((post, index) => <li key={index}>{post.title}</li>)}
       </ul>
     )
   }
+}
+
+Posts.PropTypes = {
+  posts: PropTypes.array.isRequired
 }
