989a0661620288bbff807b16be4ba327982a425b
Amanda George <github email address>
2017-10-02 09:54:05 -0500

    add navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import NavBar from '../src/components/NavBar';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Feed from './components/Feed';
+import Details from './components/Details';
 
 class App extends Component {
   render() {
@@ -9,9 +16,18 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">My Reddit</h1>
         </header>
-        <p className="App-intro">
-          <Home />
-        </p>
+        <body>
+          <div>
+            <Router>
+                <div>
+                  <NavBar />
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/feed" component={Feed} />
+                  <Route exact path="/details" component={Details} />
+                </div>
+            </Router>
+          </div>
+        </body>
       </div>
     );
   }
