d719038dc4982f3dd4086d7ba0ed2da9a3a97804
Amanda George <github email address>
2017-10-03 09:27:32 -0500

    edit out logger from configure store

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger'
 import rootReducer from './reducers'
 
-const loggerMiddleware = createLogger()
-
-export default function configureStore(preloadedState) {
+export default function configureStore() {
   return createStore(
     rootReducer,
-    preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    )
+    applyMiddleware(thunk)
   )
 }
