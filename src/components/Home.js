f72e2f618e6b70c259e3143b40de83bc81840b76
Amanda George <github email address>
2017-10-02 19:32:03 -0500

    style changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ import { Button } from 'react-bootstrap';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to My Reddit</h1>
+    <header className="App-header">
+      <h1 className="App-title">WELCOME TO MY REDDIT</h1>
+    </header>
       <Button bsStyle="success" bsSize="small" onClick={Home}>
         View Posts
       </Button>
